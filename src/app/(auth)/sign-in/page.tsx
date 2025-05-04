ef30368ebe9462781348a24dcd52d22d428a7f04
Pralad02210217 <praladmishra3@gmail.com>
2025-05-04 19:39:19 +0600

    middleware enabled

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -57,8 +57,7 @@ export default function Login() {
         const userRole = response.data?.user?.role;
           // Check for callbackUrl in the query parameters
           const searchParams = new URLSearchParams(window.location.search);
-          // const callbackUrl = searchParams.get('callbackUrl');
-          const callbackUrl = false
+          const callbackUrl = searchParams.get('callbackUrl');
           if (callbackUrl) {
             router.replace(callbackUrl);
           } else {
